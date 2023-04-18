47b7758b6e379f63ce4fae5882cdb8d9cebc8a3e
abhijeetpalanki <ajpalanki@gmail.com>
2023-04-18 15:48:30 -0500

    removed additional file for imports

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,112 +1,110 @@
-import {
-  ExpandingCard,
-  RotatingNavigation,
-  BlurryLoading,
-  ScrollAnimation,
-  SplitLandingPage,
-  SoundBoard,
-  DadJokes,
-  FAQ,
-  RandomNumberPicker,
-  AnimatedNavigation,
-  Pomodoro,
-  MoviesHub,
-  BackgroundImageSlider,
-  RockPaperScissors,
-  Canvas,
-  DoubleVerticalSlider,
-  GithubProfiles,
-  PasswordGenerator,
-  StickyNotes,
-  Screenshot,
-  Hoverboard,
-  Pokedex,
-  MagicBox,
-  LiveUserFilter,
-  Feedback,
-  Quiz,
-  Todos,
-  Relaxer,
-  Typer,
-  Horoscope,
-  QRCodeGenerator,
-  AlarmClock,
-  WordAssociations,
-  ContactUsForm,
-  UnsplashInfiniteGallery,
-  BasicCalculator,
-  Dictionary,
-  PasswordStrengthIndicator,
-  URLShortener,
-  RollTheDice,
-  AgeCalculator,
-  TheWeatherChannel,
-  Doraemon,
-  AnimatedModal,
-  HorizonalImageCarousel,
-  Image2TextConverter,
-  DiscordNavbar,
-  NewsletterLandingPage,
-  WikiSeeker,
-  DrumKit,
-  ColorPicker,
-  TypingAnimations,
-  ImagePreview,
-  AnimatedLandingPage,
-  HackerTyper,
-  HexGuesser,
-  Musification,
-  MultiStepForm,
-  RabbitLoader,
-  Focus,
-  TextGenerator,
-  CryptoPriceTracker,
-  SimpleInterestCalculator,
-  ServicesCard,
-  Notifications,
-  MemoryGame,
-  SphericalGallery,
-  Toastify,
-  BMICalculator,
-  PalindromeChecker,
-  BookHub,
-  ChatGPT,
-  CurrencyConverter,
-  Ranker,
-  MemeGenerator,
-  FootballLiveScore,
-  TipCalculator,
-  UniversityFinder,
-  Captcha,
-  EmotePicker,
-  HeroSlider,
-  ColorfulMousePointer,
-  PlaySnake,
-  MapsMarker,
-  VideoPlayer,
-  LinkTree,
-  OrbitalLoadingPage,
-  GoogleLoginButton,
-  AIKeywordExtractor,
-  MotionActivatedCard,
-  Connect4,
-  MysticSquare,
-  ThreejsBackgroundAnimation,
-  ComingSoon,
-  Hangman,
-  LightsOut,
-  ThreeDimensionWorld,
-  SkeletonLayout,
-  TrelloAnimatedChecklist,
-  GradientGenerator,
-  LineChart,
-  CustomCalendar,
-  IncomeTracker,
-  DraggableList,
-  Timeline,
-  BinaryClock,
-  AppleMessages,
-} from "../projects";
+import ExpandingCard from "../projects/Expanding Cards/ExpandingCard";
+import RotatingNavigation from "../projects/Rotating Navigation/RotatingNavigation";
+import BlurryLoading from "../projects/Blurry Loading/BlurryLoading";
+import ScrollAnimation from "../projects/Scroll Animation/ScrollAnimation";
+import SplitLandingPage from "../projects/Split Landing Page/SplitLandingPage";
+import SoundBoard from "../projects/Sound Board/SoundBoard";
+import DadJokes from "../projects/Dad Jokes/DadJokes";
+import FAQ from "../projects/FAQ Collapse/FAQ";
+import RandomNumberPicker from "../projects/Random Number Picker/RandomNumberPicker";
+import AnimatedNavigation from "../projects/Animated Navigation/AnimatedNavigation";
+import MoviesHub from "../projects/Movies Hub/MoviesHub";
+import BackgroundImageSlider from "../projects/Background Image Slider/BackgroundImageSlider";
+import RockPaperScissors from "../projects/Rock Paper Scissors/RockPaperScissors";
+import Canvas from "../projects/Canvas/Canvas";
+import DoubleVerticalSlider from "../projects/Double Vertical Slider/DoubleVerticalSlider";
+import GithubProfiles from "../projects/Github Profiles/GithubProfiles";
+import PasswordGenerator from "../projects/Password Generator/PasswordGenerator";
+import StickyNotes from "../projects/Sticky Notes/StickyNotes";
+import Screenshot from "../projects/Screenshot/Screenshot";
+import Pomodoro from "../projects/Pomodoro/PomodoroWrapper";
+import Hoverboard from "../projects/Hoverboard/Hoverboard";
+import Pokedex from "../projects/Pokedex/Pokedex";
+import MagicBox from "../projects/Magic Box/MagicBox";
+import LiveUserFilter from "../projects/Live User Filter/LiveUserFilter";
+import Feedback from "../projects/Feedback/Feedback";
+import Quiz from "../projects/Quiz/Quiz";
+import Todos from "../projects/Todos/Todos";
+import Relaxer from "../projects/Relaxer/Relaxer";
+import Typer from "../projects/Typer/Typer";
+import Horoscope from "../projects/The Daily Horoscope/Horoscope";
+import QRCodeGenerator from "../projects/QR Code Generator/QRCodeGenerator";
+import AlarmClock from "../projects/Alarm Clock/AlarmClock";
+import WordAssociations from "../projects/Word Associations/WordAssociations";
+import ContactUsForm from "../projects/Contact Us Form/ContactUsForm";
+import UnsplashInfiniteGallery from "../projects/Unsplash Infinite Gallery/UnsplashInfiniteGallery";
+import BasicCalculator from "../projects/Basic Calculator/BasicCalculator";
+import Dictionary from "../projects/Dictionary/Dictionary";
+import PasswordStrengthIndicator from "../projects/Password Strength Indicator/PasswordStrengthIndicator";
+import URLShortener from "../projects/URL Shortener/URLShortener";
+import RollTheDice from "../projects/Roll The Dice/RollTheDice";
+import AgeCalculator from "../projects/Age Calculator/AgeCalculator";
+import TheWeatherChannel from "../projects/The Weather Channel/TheWeatherChannel";
+import Doraemon from "../projects/Doraemon/Doraemon";
+import AnimatedModal from "../projects/Animated Modal/AnimatedModal";
+import HorizonalImageCarousel from "../projects/Horizonal Image Carousel/HorizonalImageCarousel";
+import Image2TextConverter from "../projects/Image 2 Text Converter/Image2TextConverter";
+import DiscordNavbar from "../projects/Discord Navbar/DiscordNavbar";
+import NewsletterLandingPage from "../projects/Newsletter Landing Page/NewsletterLandingPage";
+import WikiSeeker from "../projects/Wiki Seeker/WikiSeeker";
+import DrumKit from "../projects/Drum Kit/DrumKit";
+import ColorPicker from "../projects/Color Picker/ColorPicker";
+import TypingAnimations from "../projects/Typing Animations/TypingAnimations";
+import ImagePreview from "../projects/Image Preview/ImagePreview";
+import AnimatedLandingPage from "../projects/Animated Landing Page/AnimatedLandingPage";
+import HackerTyper from "../projects/Hacker Typer/HackerTyper";
+import HexGuesser from "../projects/Hex Guesser/HexGuesser";
+import Musification from "../projects/Musification/Musification";
+import MultiStepForm from "../projects/MultiStepForm/MultiStepForm";
+import RabbitLoader from "../projects/Rabbit Loader/RabbitLoader";
+import Focus from "../projects/Focus/Focus";
+import TextGenerator from "../projects/Text Generator/TextGenerator";
+import CryptoPriceTracker from "../projects/Crypto Price Tracker/CryptoPriceTracker";
+import SimpleInterestCalculator from "../projects/Simple Interest Calculator/SimpleInterestCalculator";
+import ServicesCard from "../projects/Services Card/ServicesCard";
+import Notifications from "../projects/Notifications/Notifications";
+import MemoryGame from "../projects/Memory Game/MemoryGame";
+import SphericalGallery from "../projects/Spherical Gallery/SphericalGallery";
+import Toastify from "../projects/Toastify/Toastify";
+import BMICalculator from "../projects/BMI Calculator/BMICalculator";
+import PalindromeChecker from "../projects/Palindrome Checker/PalindromeChecker";
+import BookHub from "../projects/Book Hub/BookHub";
+import ChatGPT from "../projects/ChatGPT Bot/ChatGPT";
+import CurrencyConverter from "../projects/Currency Converter/CurrencyConverter";
+import Ranker from "../projects/Ranker/Ranker";
+import MemeGenerator from "../projects/Meme Generator/MemeGenerator";
+import FootballLiveScore from "../projects/Football Live Score/FootballLiveScore";
+import TipCalculator from "../projects/Tip Calculator/TipCalculator";
+import UniversityFinder from "../projects/University Finder/UniversityFinder";
+import Captcha from "../projects/Captcha/Captcha";
+import EmotePicker from "../projects/Emote Picker/EmotePicker";
+import HeroSlider from "../projects/Hero Slider/HeroSlider";
+import ColorfulMousePointer from "../projects/Colorful Mouse Pointer/ColorfulMousePointer";
+import PlaySnake from "../projects/Play Snake/PlaySnake";
+import MapsMarker from "../projects/Maps Marker/MapsMarker";
+import VideoPlayer from "../projects/Video Player/VideoPlayer";
+import LinkTree from "../projects/Link Tree/LinkTree";
+import OrbitalLoadingPage from "../projects/Orbital Loading Page/OrbitalLoadingPage";
+import GoogleLoginButton from "../projects/Google Login Button/GoogleLoginButton";
+import AIKeywordExtractor from "../projects/AI Keyword Extractor/AIKeywordExtractor";
+import MotionActivatedCard from "../projects/Motion Activated Card/MotionActivatedCard";
+import Connect4 from "../projects/Connect 4/Connect4";
+import MysticSquare from "../projects/Mystic Square/MysticSquare";
+import ThreejsBackgroundAnimation from "../projects/Three.js Background Animation/ThreejsBackgroundAnimation";
+import ComingSoon from "../projects/Coming Soon/ComingSoon";
+import Hangman from "../projects/Hangman/Hangman";
+import LightsOut from "../projects/Lights Out/LightsOut";
+import ThreeDimensionWorld from "../projects/Three Dimension World/ThreeDimensionWorld";
+import SkeletonLayout from "../projects/Skeleton Layout/SkeletonLayout";
+import TrelloAnimatedChecklist from "../projects/Trello Animated Checklist/TrelloAnimatedChecklist";
+import GradientGenerator from "../projects/Gradient Generator/GradientGenerator";
+import LineChart from "../projects/Line Chart/LineChart";
+import CustomCalendar from "../projects/Custom Calendar/CustomCalendar";
+import IncomeTracker from "../projects/Income Tracker/IncomeTracker";
+import DraggableList from "../projects/Draggable List/DraggableList";
+import Timeline from "../projects/Timeline/Timeline";
+import BinaryClock from "../projects/Binary Clock/BinaryClock";
+import AppleMessages from "../projects/Apple Messages/AppleMessages";
 
 export const projects = [
   {
